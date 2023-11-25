f0ef13bb5f5c7ea92a1053724753f868c7a44729
Rupesh sahu <rupeshsahu969@gmail.com>
2023-11-25 11:01:48 +0530

    add new data and hme page s

--- app/post/Home.js
+++ app/post/Home.js
@@ -3,7 +3,9 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1> Helo worlds </h1>
+      <h1> Helo worlds
+       </h1>
+       <p> I am home apges </p>
     </div>
   )
 }
