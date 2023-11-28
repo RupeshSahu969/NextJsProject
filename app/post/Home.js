8de3350cff8bb83a5ba587c2cba21c8d8b537d73
Rupesh sahu <rupeshsahu969@gmail.com>
2023-11-28 08:35:43 +0530

    add

--- app/post/Home.js
+++ app/post/Home.js
@@ -3,12 +3,7 @@ import React from 'react'
 const Home = () => {
   return (
     <div>
-      <h1> Helo worlds
-       </h1>
-       <p> I am home apges </p>
-       <p>lorem
-        
-       </p>
+      
     </div>
   )
 }
